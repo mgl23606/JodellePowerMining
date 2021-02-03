06b7bc58af2efaa6f5b768041b5aad923df8aa27
mgl23606 <mgl23606@gmail.com>
2021-02-03 22:55:47 +0000

    Altered the way recipes are created to use the config file. Also applied inheritance.

--- src/main/java/jodelle/powermining/crafting/CraftItemExcavator.java
+++ src/main/java/jodelle/powermining/crafting/CraftItemExcavator.java
@@ -12,175 +12,46 @@
 
 package jodelle.powermining.crafting;
 
-import jodelle.powermining.PowerMining;
-import jodelle.powermining.enchantment.Glow;
+import jodelle.powermining.lib.Reference;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.util.Map;
 
-public class CraftItemExcavator {
-	public JavaPlugin plugin;
+public class CraftItemExcavator extends CraftItem {
+
 	public static String loreString = "POUND!";
 
-	ItemStack WoodExcavator = new ItemStack(Material.WOODEN_SHOVEL, 1);
-	ItemStack StoneExcavator = new ItemStack(Material.STONE_SHOVEL, 1);
-	ItemStack IronExcavator = new ItemStack(Material.IRON_SHOVEL, 1);
-	ItemStack GoldExcavator = new ItemStack(Material.GOLDEN_SHOVEL, 1);
-	ItemStack DiamondExcavator = new ItemStack(Material.DIAMOND_SHOVEL, 1);
-	ItemStack NetheriteExcavator = new ItemStack(Material.NETHERITE_SHOVEL, 1);
-
-	ShapedRecipe WoodExcavatorRecipe;
-	ShapedRecipe WoodExcavatorBirchRecipe;
-	ShapedRecipe WoodExcavatorAcaciaRecipe;
-	ShapedRecipe WoodExcavatorJungleRecipe;
-	ShapedRecipe WoodExcavatorDarkOakRecipe;
-	ShapedRecipe WoodExcavatorSpurceRecipe;
-
-	ShapedRecipe StoneExcavatorRecipe;
-	ShapedRecipe IronExcavatorRecipe;
-	ShapedRecipe GoldExcavatorRecipe;
-	ShapedRecipe DiamondExcavatorRecipe;
-	ShapedRecipe NetheriteExcavatorRecipe;
-
 	public CraftItemExcavator(JavaPlugin plugin) {
+		ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
 		this.plugin = plugin;
+		for(Map.Entry<String, ItemStack[]> tool : Reference.EXCAVATOR_CRAFTING_RECIPES.entrySet()){
 
-		modifyItemMeta();
-		setRecipes();
-		registerRecipes();
-	}
+			//key is the name of the powertool. Ex: DIAMOND_HAMMER
+			//value is an array containing the recipe
+			String key = tool.getKey();
+			ItemStack[] value = tool.getValue();
+			console.sendMessage(ChatColor.AQUA + "Creating: " + key);
+			//We start by finding the position of the name on the HAMMERS array
+			//With that position we can fetch the name of the minecraft item present in other array
+			int i = Reference.EXCAVATORS.indexOf(key);
+			console.sendMessage(ChatColor.AQUA + String.valueOf(i));
+			Material pickaxe = Reference.SHOVELS.get(i);
 
-	// Get metadata for all excavator types, add lore and change the names to identify them as excavators
-	public void modifyItemMeta() {
+			ItemStack powerTool = new ItemStack(pickaxe, 1);
+			//powerTools.add(powerTool);
 
-		ItemMeta WoodExcavatorMeta = WoodExcavator.getItemMeta();
-		ItemMeta StoneExcavatorMeta = StoneExcavator.getItemMeta();
-		ItemMeta IronExcavatorMeta = IronExcavator.getItemMeta();
-		ItemMeta GoldExcavatorMeta = GoldExcavator.getItemMeta();
-		ItemMeta DiamondExcavatorMeta = DiamondExcavator.getItemMeta();
-		ItemMeta NetheriteExcavatorMeta = NetheriteExcavator.getItemMeta();
+			modifyItemMeta(powerTool, loreString);
 
-		NamespacedKey isPowerTool = new NamespacedKey(PowerMining.getInstance(), "isPowerTool");
-		WoodExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		StoneExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		IronExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		GoldExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		DiamondExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		NetheriteExcavatorMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
+			ShapedRecipe recipe = createRecipe(powerTool, key, value);
 
-		ArrayList<String> lore = new ArrayList<String>();
-		lore.add(loreString);
+			registerRecipes(recipe);
 
-		WoodExcavatorMeta.setDisplayName("Wooden Excavator");
-		StoneExcavatorMeta.setDisplayName("Stone Excavator");
-		IronExcavatorMeta.setDisplayName("Iron Excavator");
-		GoldExcavatorMeta.setDisplayName("Golden Excavator");
-		DiamondExcavatorMeta.setDisplayName("Diamond Excavator");
-		NetheriteExcavatorMeta.setDisplayName("Netherite Excavator");
-
-		WoodExcavatorMeta.setLore(lore);
-		WoodExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"WoodExcavator")),  1, false);
-
-		StoneExcavatorMeta.setLore(lore);
-		StoneExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"StoneExcavator")),  1, false);
-
-		IronExcavatorMeta.setLore(lore);
-		IronExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"IronExcavator")),  1, false);
-
-		GoldExcavatorMeta.setLore(lore);
-		GoldExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"GoldExcavator")),  1, false);
-
-		DiamondExcavatorMeta.setLore(lore);
-		DiamondExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"DiamondExcavator")),  1, false);
-
-		NetheriteExcavatorMeta.setLore(lore);
-		NetheriteExcavatorMeta.addEnchant(new Glow(new NamespacedKey(plugin,"NetheriteExcavator")),  1, false);
-
-		WoodExcavator.setItemMeta(WoodExcavatorMeta);
-		StoneExcavator.setItemMeta(StoneExcavatorMeta);
-		IronExcavator.setItemMeta(IronExcavatorMeta);
-		GoldExcavator.setItemMeta(GoldExcavatorMeta);
-		DiamondExcavator.setItemMeta(DiamondExcavatorMeta);
-		NetheriteExcavator.setItemMeta(NetheriteExcavatorMeta);
-	}
-
-	// Creates the ShapedRecipe patterns for all excavator types
-	public void setRecipes() {
-		WoodExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavator"),WoodExcavator);
-		WoodExcavatorBirchRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavatorBirchRecipe"),WoodExcavator);
-		WoodExcavatorAcaciaRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavatorAcaciaRecipe"),WoodExcavator);
-		WoodExcavatorJungleRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavatorJungleRecipe"),WoodExcavator);
-		WoodExcavatorDarkOakRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavatorDarkOakRecipe"),WoodExcavator);
-		WoodExcavatorSpurceRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodExcavatorSpurceRecipe"),WoodExcavator);
-		StoneExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin,"StoneExcavator"), StoneExcavator);
-		IronExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin,"IronExcavator"),IronExcavator);
-		GoldExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin,"GoldExcavator"),GoldExcavator);
-		DiamondExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin,"DiamondExcavator"),DiamondExcavator);
-		NetheriteExcavatorRecipe = new ShapedRecipe(new NamespacedKey(plugin,"NetheriteExcavator"),NetheriteExcavator);
-
-		WoodExcavatorRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorRecipe.setIngredient('m', Material.OAK_LOG);
-		WoodExcavatorRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		WoodExcavatorBirchRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorBirchRecipe.setIngredient('m', Material.BIRCH_LOG);
-		WoodExcavatorBirchRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		WoodExcavatorAcaciaRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorAcaciaRecipe.setIngredient('m', Material.ACACIA_LOG);
-		WoodExcavatorAcaciaRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		WoodExcavatorJungleRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorJungleRecipe.setIngredient('m', Material.JUNGLE_LOG);
-		WoodExcavatorJungleRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		WoodExcavatorDarkOakRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorDarkOakRecipe.setIngredient('m', Material.DARK_OAK_LOG);
-		WoodExcavatorDarkOakRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		WoodExcavatorSpurceRecipe.shape(" m ", "mim", " m ");
-		WoodExcavatorSpurceRecipe.setIngredient('m', Material.SPRUCE_LOG);
-		WoodExcavatorSpurceRecipe.setIngredient('i', Material.WOODEN_SHOVEL);
-
-		StoneExcavatorRecipe.shape(" m ", "mim", " m ");
-		StoneExcavatorRecipe.setIngredient('m', Material.STONE);
-		StoneExcavatorRecipe.setIngredient('i', Material.STONE_SHOVEL);
-
-		IronExcavatorRecipe.shape(" m ", "mim", " m ");
-		IronExcavatorRecipe.setIngredient('m', Material.IRON_INGOT);
-		IronExcavatorRecipe.setIngredient('i', Material.IRON_SHOVEL);
-
-		GoldExcavatorRecipe.shape(" m ", "mim", " m ");
-		GoldExcavatorRecipe.setIngredient('m', Material.GOLD_INGOT);
-		GoldExcavatorRecipe.setIngredient('i', Material.GOLDEN_SHOVEL);
-
-		DiamondExcavatorRecipe.shape(" m ", "mim", " m ");
-		DiamondExcavatorRecipe.setIngredient('m', Material.DIAMOND);
-		DiamondExcavatorRecipe.setIngredient('i', Material.DIAMOND_SHOVEL);
-
-		NetheriteExcavatorRecipe.shape(" m ", "mim", " m ");
-		NetheriteExcavatorRecipe.setIngredient('m', Material.NETHERITE_INGOT);
-		NetheriteExcavatorRecipe.setIngredient('i', Material.NETHERITE_SHOVEL);
-	}
-
-	// Registers all created recipes into the game
-	public void registerRecipes() {
-		plugin.getServer().addRecipe(WoodExcavatorRecipe);
-		plugin.getServer().addRecipe(WoodExcavatorBirchRecipe);
-		plugin.getServer().addRecipe(WoodExcavatorAcaciaRecipe);
-		plugin.getServer().addRecipe(WoodExcavatorJungleRecipe);
-		plugin.getServer().addRecipe(WoodExcavatorDarkOakRecipe);
-		plugin.getServer().addRecipe(WoodExcavatorSpurceRecipe);
-		plugin.getServer().addRecipe(StoneExcavatorRecipe);
-		plugin.getServer().addRecipe(IronExcavatorRecipe);
-		plugin.getServer().addRecipe(GoldExcavatorRecipe);
-		plugin.getServer().addRecipe(DiamondExcavatorRecipe);
-		plugin.getServer().addRecipe(NetheriteExcavatorRecipe);
+		}
 	}
 }
--- src/main/java/jodelle/powermining/crafting/CraftItemHammer.java
+++ src/main/java/jodelle/powermining/crafting/CraftItemHammer.java
@@ -13,174 +13,52 @@
 package jodelle.powermining.crafting;
 
 
-import jodelle.powermining.PowerMining;
-import jodelle.powermining.enchantment.Glow;
+import jodelle.powermining.lib.Reference;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.util.Map;
 
-public class CraftItemHammer {
-	public JavaPlugin plugin;
+public class CraftItemHammer extends CraftItem{
+
 	public static String loreString = "SMASH!";
 
-	ItemStack WoodHammer = new ItemStack(Material.WOODEN_PICKAXE, 1);
-	ItemStack StoneHammer = new ItemStack(Material.STONE_PICKAXE, 1);
-	ItemStack IronHammer = new ItemStack(Material.IRON_PICKAXE, 1);
-	ItemStack GoldHammer = new ItemStack(Material.GOLDEN_PICKAXE, 1);
-	ItemStack DiamondHammer = new ItemStack(Material.DIAMOND_PICKAXE, 1);
-	ItemStack NetheriteHammer = new ItemStack(Material.NETHERITE_PICKAXE, 1);
-
-	ShapedRecipe WoodHammerRecipe;
-	ShapedRecipe WoodHammerOakRecipe;
-	ShapedRecipe WoodHammerDarkOakRecipe;
-	ShapedRecipe WoodHammerJungleRecipe;
-	ShapedRecipe WoodHammerBirchRecipe;
-	ShapedRecipe WoodHammerAcaciaRecipe;
-	ShapedRecipe StoneHammerRecipe;
-	ShapedRecipe IronHammerRecipe;
-	ShapedRecipe GoldHammerRecipe;
-	ShapedRecipe DiamondHammerRecipe;
-	ShapedRecipe NetheriteHammerRecipe;
-
 	public CraftItemHammer(JavaPlugin plugin) {
+		ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
 		this.plugin = plugin;
+		for(Map.Entry<String, ItemStack[]> tool : Reference.HAMMER_CRAFTING_RECIPES.entrySet()){
 
-		modifyItemMeta();
-		setRecipes();
-		registerRecipes();
+			//key is the name of the powertool. Ex: DIAMOND_HAMMER
+			//value is an array containing the recipe
+			String key = tool.getKey();
+			ItemStack[] value = tool.getValue();
+			console.sendMessage(ChatColor.AQUA + "Creating: " + key);
+			//We start by finding the position of the name on the HAMMERS array
+			//With that position we can fetch the name of the minecraft item present in other array
+			int i = Reference.HAMMERS.indexOf(key);
+			console.sendMessage(ChatColor.AQUA + String.valueOf(i));
+			Material pickaxe = Reference.PICKAXES.get(i);
+
+			ItemStack powerTool = new ItemStack(pickaxe, 1);
+			//powerTools.add(powerTool);
+
+			modifyItemMeta(powerTool, loreString);
+
+			ShapedRecipe recipe = createRecipe(powerTool, key, value);
+
+			registerRecipes(recipe);
+		}
+
 	}
 
-	// Get metadata for all hammer types, add lore and change the names to identify them as hammers
-	public void modifyItemMeta() {
-		ItemMeta WoodHammerMeta = WoodHammer.getItemMeta();
-		ItemMeta StoneHammerMeta = StoneHammer.getItemMeta();
-		ItemMeta IronHammerMeta = IronHammer.getItemMeta();
-		ItemMeta GoldHammerMeta = GoldHammer.getItemMeta();
-		ItemMeta DiamondHammerMeta = DiamondHammer.getItemMeta();
-		ItemMeta NetheriteHammerMeta = NetheriteHammer.getItemMeta();
 
-		ArrayList<String> lore = new ArrayList<String>();
-		lore.add(loreString);
 
-		WoodHammerMeta.setDisplayName("Wooden Hammer");
-		StoneHammerMeta.setDisplayName("Stone Hammer");
-		IronHammerMeta.setDisplayName("Iron Hammer");
-		GoldHammerMeta.setDisplayName("Golden Hammer");
-		DiamondHammerMeta.setDisplayName("Diamond Hammer");
-		NetheriteHammerMeta.setDisplayName("Netherite Hammer");
 
-		NamespacedKey isPowerTool = new NamespacedKey(PowerMining.getInstance(), "isPowerTool");
-		WoodHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		StoneHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		IronHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		GoldHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		DiamondHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
-		NetheriteHammerMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
 
-		WoodHammerMeta.setLore(lore);
-		WoodHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"WoodHammer")),  1, false);
-
-		StoneHammerMeta.setLore(lore);
-		StoneHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"StoneHammer")),  1, false);
-
-		IronHammerMeta.setLore(lore);
-		IronHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"IronHammer")),  1, false);
-
-		GoldHammerMeta.setLore(lore);
-		GoldHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"GoldHammer")),  1, false);
-
-		DiamondHammerMeta.setLore(lore);
-		DiamondHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"DiamondHammer")),  1, false);
-
-		NetheriteHammerMeta.setLore(lore);
-		NetheriteHammerMeta.addEnchant(new Glow(new NamespacedKey(plugin,"NetheriteHammer")),  1, false);
-
-		WoodHammer.setItemMeta(WoodHammerMeta);
-		StoneHammer.setItemMeta(StoneHammerMeta);
-		IronHammer.setItemMeta(IronHammerMeta);
-		GoldHammer.setItemMeta(GoldHammerMeta);
-		DiamondHammer.setItemMeta(DiamondHammerMeta);
-		NetheriteHammer.setItemMeta(NetheriteHammerMeta);
-	}
-
-	// Creates the ShapedRecipe patterns for all hammer types
-	public void setRecipes() {
-		WoodHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodHammer"), WoodHammer);
-		WoodHammerOakRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodOakHammer"), WoodHammer);
-		WoodHammerDarkOakRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodDarkOakHammer"), WoodHammer);
-		WoodHammerJungleRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodHammerJungle"), WoodHammer);
-		WoodHammerBirchRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodHammerBirch"), WoodHammer);
-		WoodHammerAcaciaRecipe = new ShapedRecipe(new NamespacedKey(plugin, "WoodHammerAcacia"), WoodHammer);
-		StoneHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "StoneHammer"), StoneHammer);
-		IronHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "IronHammer"), IronHammer);
-		GoldHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "GoldHammer"), GoldHammer);
-		DiamondHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "DiamondHammer"), DiamondHammer);
-		NetheriteHammerRecipe = new ShapedRecipe(new NamespacedKey(plugin, "NetheriteHammer"), NetheriteHammer);
-
-		WoodHammerRecipe.shape(" m ", "mim", " m ");
-		WoodHammerRecipe.setIngredient('m', Material.SPRUCE_LOG);
-		WoodHammerRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		WoodHammerOakRecipe.shape(" m ", "mim", " m ");
-		WoodHammerOakRecipe.setIngredient('m', Material.OAK_LOG);
-		WoodHammerOakRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		WoodHammerDarkOakRecipe.shape(" m ", "mim", " m ");
-		WoodHammerDarkOakRecipe.setIngredient('m', Material.DARK_OAK_LOG);
-		WoodHammerDarkOakRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		WoodHammerJungleRecipe.shape(" m ", "mim", " m ");
-		WoodHammerJungleRecipe.setIngredient('m', Material.JUNGLE_LOG);
-		WoodHammerJungleRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		WoodHammerBirchRecipe.shape(" m ", "mim", " m ");
-		WoodHammerBirchRecipe.setIngredient('m', Material.BIRCH_LOG);
-		WoodHammerBirchRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		WoodHammerAcaciaRecipe.shape(" m ", "mim", " m ");
-		WoodHammerAcaciaRecipe.setIngredient('m', Material.ACACIA_LOG);
-		WoodHammerAcaciaRecipe.setIngredient('i', Material.WOODEN_PICKAXE);
-
-		StoneHammerRecipe.shape(" m ", "mim", " m ");
-		StoneHammerRecipe.setIngredient('m', Material.STONE);
-		StoneHammerRecipe.setIngredient('i', Material.STONE_PICKAXE);
-
-		IronHammerRecipe.shape(" m ", "mim", " m ");
-		IronHammerRecipe.setIngredient('m', Material.IRON_INGOT);
-		IronHammerRecipe.setIngredient('i', Material.IRON_PICKAXE);
-
-		GoldHammerRecipe.shape(" m ", "mim", " m ");
-		GoldHammerRecipe.setIngredient('m', Material.GOLD_INGOT);
-		GoldHammerRecipe.setIngredient('i', Material.GOLDEN_PICKAXE);
-
-		DiamondHammerRecipe.shape(" m ", "mim", " m ");
-		DiamondHammerRecipe.setIngredient('m', Material.DIAMOND);
-		DiamondHammerRecipe.setIngredient('i', Material.DIAMOND_PICKAXE);
-
-		NetheriteHammerRecipe.shape(" m ", "mim", " m ");
-		NetheriteHammerRecipe.setIngredient('m', Material.NETHERITE_INGOT);
-		NetheriteHammerRecipe.setIngredient('i', Material.NETHERITE_PICKAXE);
-	}
-
-	// Registers all created recipes into the game
-	public void registerRecipes() {
-		plugin.getServer().addRecipe(WoodHammerRecipe);
-		plugin.getServer().addRecipe(WoodHammerOakRecipe);
-		plugin.getServer().addRecipe(WoodHammerDarkOakRecipe);
-		plugin.getServer().addRecipe(WoodHammerJungleRecipe);
-		plugin.getServer().addRecipe(WoodHammerBirchRecipe);
-		plugin.getServer().addRecipe(WoodHammerAcaciaRecipe);
-		plugin.getServer().addRecipe(StoneHammerRecipe);
-		plugin.getServer().addRecipe(IronHammerRecipe);
-		plugin.getServer().addRecipe(GoldHammerRecipe);
-		plugin.getServer().addRecipe(DiamondHammerRecipe);
-		plugin.getServer().addRecipe(NetheriteHammerRecipe);
-	}
 
 }
