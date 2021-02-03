90be6f2ba272456d6539cda81eab9cd71ba89b75
mgl23606 <mgl23606@gmail.com>
2021-02-03 22:59:00 +0000

    fixup! Altered the way recipes are created to use the config file. Also applied inheritance.

---
+++ src/main/java/jodelle/powermining/crafting/CraftItem.java
@@ -0,0 +1,64 @@
+package jodelle.powermining.crafting;
+
+
+import jodelle.powermining.PowerMining;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+public class CraftItem {
+
+    ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+    protected JavaPlugin plugin;
+
+    protected ItemStack modifyItemMeta(ItemStack powerTool, String loreString){
+        ItemMeta powerToolMeta = powerTool.getItemMeta();
+
+        NamespacedKey isPowerTool = new NamespacedKey(PowerMining.getInstance(), "isPowerTool");
+        powerToolMeta.getPersistentDataContainer().set(isPowerTool, PersistentDataType.INTEGER, 1);
+
+        ArrayList<String> lore = new ArrayList<String>();
+        lore.add(loreString);
+
+        powerToolMeta.setDisplayName(loreString);
+
+        powerToolMeta.setLore(lore);
+
+        powerTool.setItemMeta(powerToolMeta);
+
+        return powerTool;
+    }
+
+    protected ShapedRecipe createRecipe(ItemStack powerTool, String name, ItemStack[] recipe){
+        //Initialize the recipe
+        ShapedRecipe toolRecipe = new ShapedRecipe(new NamespacedKey(plugin, name),powerTool);
+        console.sendMessage(ChatColor.AQUA + "NameSpacedKey:" + name);
+        char[] alphabet = new char[]{
+                'a','b','c','d','e','f','g','h','i'
+        };
+
+        toolRecipe.shape("abc", "def", "ghi");
+        for (int i = 0; i < 9; i++) {
+            if (recipe[i] != null) {
+                toolRecipe.setIngredient(alphabet[i], recipe[i].getType());
+            }
+        }
+
+        return toolRecipe;
+
+    }
+
+    protected void registerRecipes(ShapedRecipe recipe) {
+        console.sendMessage(ChatColor.AQUA + "Adding Recipe:" + recipe.getKey().getKey());
+        plugin.getServer().addRecipe(recipe);
+    }
+
+}
