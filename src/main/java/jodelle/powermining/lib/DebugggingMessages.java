6eb9b58a46b229d357219dbb9d75c2331ec8fe16
mgl23606 <mgl23606@gmail.com>
2021-06-03 12:46:24 +0100

    Class to handle debugging messages

---
+++ src/main/java/jodelle/powermining/lib/DebugggingMessages.java
@@ -0,0 +1,32 @@
+package jodelle.powermining.lib;
+
+import com.palmergames.bukkit.TownyChat.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+
+public class DebugggingMessages {
+
+    protected boolean debugginOn = true;
+    protected ConsoleCommandSender console;
+
+    public DebugggingMessages() {
+        if (debugginOn){
+            console = Bukkit.getServer().getConsoleSender();
+            sendConsoleMessage("Debugging is On");
+        }
+    }
+
+    public void sendConsoleMessage(String message){
+        if (debugginOn){
+            console.sendMessage( ChatColor.LIGHT_PURPLE + "[JodellePowerMiningDebugging] - " + message);
+        }
+    }
+
+    public void sendConsoleMessage(Boolean show, String message){
+        if (debugginOn && show){
+            console.sendMessage( ChatColor.LIGHT_PURPLE + "[JodellePowerMiningDebugging] - " + message);
+        }
+    }
+
+}
